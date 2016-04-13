8182ae9fd7669e9743c413e339ea8d4064efa697
Cef Ramirez <cef@Cefs-MacBook-Pro.local>
2016-04-13 10:19:29 -0700

    supporting message publishing

--- PDMServer/routes/msg.js
+++ PDMServer/routes/msg.js
@@ -6,36 +6,31 @@ var express = require('express');
 var router = express.Router();
 var client = require('../model/client');
 
-
- 
-
 router.post('/:dueid/:appid', function(req, res, next) {
     res.setHeader('Content-Type', 'application/text');
     var messg = req.body;
-    messg = JSON.stringify(messg);
     var dueid = req.params.dueid;
     var appid =  req.params.appid;
 
-    // var topic = '/up/'+appid+'/1/'+dueid ;
-    var topic = '/down/'+appid+dueid ;
-    console.log(topic,messg);
+    console.log("received: " + JSON.stringify(messg));
 
-    client.publish(topic,messg,function(err){
-        if(err){
-            console.log(err);
-        }
-        
-        console.log("Published bitches !!" );
-        
-       client.getmessagesForDueId(req.params.dueid,function (messages) {
+    var messages = messg['msg'];
+    messages.forEach(function(o){
+        var port = o[0]; // first item should be port
+        var m = o[1]; // second item should be message string
+        var topic = '/up/'+appid+'/'+port+'/'+dueid;
+        console.log(topic, m);
+        client.publish(topic,m,function(err){
+            if(err){
+                console.log('Failed to publish message: (' + topic + ')' + err);
+            }
+        });
+    });
 
-           console.log("back here with : "+ (messages['message0']));
-
-        res.send(messages);
-       });
-    }
-    );
-
+    client.getmessagesForDueId(dueid, function(messages) {
+        //console.log("back here with : "+ (messages['message0']));
+        res.send(JSON.stringify([]));
+    });
     
 });
 
