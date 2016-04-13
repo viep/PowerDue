ec3dacd61413fc1cbd13d963545d77071fb81021
Cef Ramirez <cef@Cefs-MacBook-Pro.local>
2016-04-13 12:05:55 -0700

    switching to use due buffer class

--- PDMServer/routes/register.js
+++ PDMServer/routes/register.js
@@ -4,26 +4,34 @@ var router = express.Router();
 var client = require('../model/client');
 
 
-router.post('/:appid/:dueid', function(req, res, next) {
+router.post('/:dueid/:appid', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
     var dueid =  req.params.dueid;
     var appid =  req.params.appid;
-    var topic= '/down/'+appid+'/'+dueid;
-
-    client.subscribe(topic,function(err,granted){
-
-        if(err){
-            console.log("Error in subscription");
-            res.status(500).send("Failed to Subscribe");
+    client.addBufferForDue(dueid, appid, function(buffer){
+        console.log("here: ",buffer);
+        if(buffer !== undefined){
+            res.status(200).send(JSON.stringify({"result": "OK"}));
+        } else {
+            res.status(500).send(JSON.stringify({"error": "Failed to register"}));
         }
-        console.log("Successful subscription"+ JSON.stringify(granted));
-        client.addDuetoBuffer(req.params.dueid,req.params.appid,function(err){
+    });
+    // var topic= '/down/'+appid+'/'+dueid;
+
+    // client.subscribe(topic,function(err,granted){
+
+    //     if(err){
+    //         console.log("Error in subscription");
+    //         res.status(500).send("Failed to Subscribe");
+    //     }
+    //     console.log("Successful subscription"+ JSON.stringify(granted));
+    //     client.addDuetoBuffer(req.params.dueid,req.params.appid,function(err){
            
-            res.status(200).send("Successful subscription"+JSON.stringify(granted));
-        })
+    //         res.status(200).send("Successful subscription"+JSON.stringify(granted));
+    //     })
         
 
-    });
+    // });
 
   // res.send(JSON.stringify(ans));
   // res.send(JSON.stringify(ans));
